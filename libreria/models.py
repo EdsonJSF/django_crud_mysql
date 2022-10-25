164d4bf05910c53590ed06e27b949af7cbc1bd01
Edsonjsf <edsonjsf1990@gmail.com>
2022-10-25 13:23:01 -0400

    Propiedades en el modelo admin

--- libreria/models.py
+++ libreria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from requests import delete
 
 # Create your models here.
 class Libro(models.Model):
@@ -6,3 +7,10 @@ class Libro(models.Model):
   titulo = models.CharField(max_length=100, verbose_name='Título')
   imagen = models.ImageField(upload_to='imagenes/', null=True, verbose_name='Imagen')
   descripcion = models.TextField(null=True, verbose_name='Descripción')
+
+  def __str__(self):
+    return f"Titulo: {self.titulo} - Descripción: {self.descripcion}"
+
+  def delete(self, using=None, keep_parents=False):
+    self.imagen.storage.delete(self.imagen.name)
+    super().delete()
