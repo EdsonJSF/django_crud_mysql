a2b0676657cfdea564e7d6136119e2df55e02d1e
Edsonjsf <edsonjsf1990@gmail.com>
2022-10-27 13:21:22 -0400

    Guardando datos en crear_libro

--- libreria/views.py
+++ libreria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Libro
 from .forms import LibroForm
@@ -15,7 +15,10 @@ def libros(request):
   return render(request, 'pages/libros/index.html', {'libros': libros})
 
 def crear_libro(request):
-  formulario = LibroForm(request.POST or None)
+  formulario = LibroForm(request.POST or None, request.FILES or None)
+  if formulario.is_valid():
+    formulario.save()
+    return redirect('libros')
   return render(request, 'pages/libros/create.html', {'formulario': formulario})
 
 def editar_libro(request):
